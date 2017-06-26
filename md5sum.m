24372a21f40bc98c4cbbe10d0c36be0678d2c346
bimac <bimac@users.noreply.github.com>
2017-06-26 11:57:35 +0200

    Add files via upload
    speed improvements + documentation

--- md5sum.m
+++ md5sum.m
@@ -3,7 +3,9 @@ function out = md5sum(in)
 %   OUT = MD5SUM(FILENAME) calculates the MD5 hash value (1) of file IN
 %   using calls to system functions. For performance reasons, MD5SUM will
 %   hand over to Jan Simon's GETMD5 (2) if the latter is accessible as a
-%   compiled MEX file.
+%   compiled MEX file. The result: reliable calculation of MD5 sums on all
+%   modern platforms (GLNXA64, MACI64, PCWIN64) plus the speed of MEX files
+%   on supported systems (via GETMD5, with small overhead).
 %
 %   Example: out = md5sum('directory/file.ext')
 %
@@ -13,11 +15,18 @@ function out = md5sum(in)
 %   See also GETMD5
 
 % Revision history:
+% 2017-06-26 speed improvements + documentation
 % 2017-06-14 small speed improvements
 % 2017-06-12 initial release
 
-% Try to pass the input to Jan Simon's GetMD5 ...
-if regexpi(which('GetMD5'),[mexext '$'])
+% Check if GetMD5 is available
+persistent GetMD5available
+if isempty(GetMD5available)
+    GetMD5available = endsWith(which('GetMD5'),mexext);
+end
+
+% Pass input to GetMD5
+if GetMD5available
     out = GetMD5(in,'file');
     return
 end
@@ -25,7 +34,7 @@ end
 % Check input argument
 validateattributes(in,{'char'},{'nonempty','row'});
 if ~java.io.File(in).exists
-	error('File not found: %s',in)
+    error('File not found: %s',in)
 end
 
 % Get the MD5 sum via system calls
